8a29ef943f1ae9595b3250c604df9f862458c2a0
vutrongduong <duongvu.050898@gmail.com>
2021-10-30 17:29:40 +0700

    upbai

---
+++ src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/XoaPhanTuKhoiMang.java
@@ -0,0 +1,22 @@
+package ss3.bai_tap;
+
+import java.util.Scanner;
+
+public class XoaPhanTuKhoiMang {
+    public static void main(String[] args) {
+//        Scanner scanner =new Scanner(System.in);
+//        int[] arr={10,4,6,8,6,0,0,0,0,0};
+//        int x=scanner.nextInt();
+//        for(int i=0;i<arr.length;i++){
+//            if (x==arr[i]){
+//                System.out.println(i);
+//                arr[i+1]=
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập tên");
+        char name = scanner.next().charAt(0);
+        System.out.println(name);
+            }
+        }
+
+    }
+}
